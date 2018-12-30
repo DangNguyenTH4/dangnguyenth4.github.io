cff25c2f34436420ace4a4eb2b40742aa6853c2b
DangNguyenTH4 <dangnt520@wru.vn>
2018-12-30 16:57:28 +0700

    Xong phan kinh nghiem va qua trinh hoc tap

--- js.js
+++ js.js
@@ -1,4 +1,20 @@
 ﻿ $(function(){
- 
+ 	 	 
+ 	 $(".xuong,.kn").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoiedu').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	$(".sp").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	
+	$(".cm").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+
 })  
- 
