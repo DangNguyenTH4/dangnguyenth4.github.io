8240110a06f02bcbe632bf99bdab06e38cea4692
DangNT <dangnt520@wru.vn>
2021-08-22 18:14:35 +0700

    Update fbcontact, linked contact

--- vendor/js.js
+++ vendor/js.js
@@ -22,4 +22,13 @@ $(document).on('click','div#callme',function(){
 	document.getElementById("au").pause();
 	window.open("https://www.topcv.vn/xem-cv/BgJQD1AKBAYBUFVXW1dSB1YGUFFUBwMNVAJQWw157b")
 });
+$(document).on('click','div#linkedIn',function(){
+	document.getElementById("au").pause();
+	window.open("https://www.facebook.com/nguyen.dang.tlu")
+});
+$(document).on('click','div#facebook',function(){
+	document.getElementById("au").pause();
+	window.open("https://www.linkedin.com/in/nguyen-dang-5a981a153/")
+});
+
 
