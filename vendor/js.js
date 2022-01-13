913bec9ca94add1a8dd089739044dfc8d6059cb2
DangNT <dangnt520@wru.vn>
2022-01-13 10:00:37 +0700

    Remove Fb,Topcv link, redirect to skype or linkedin

--- vendor/js.js
+++ vendor/js.js
@@ -20,11 +20,11 @@
 })
 $(document).on('click','div#callme',function(){
 	document.getElementById("au").pause();
-	window.open("https://www.topcv.vn/xem-cv/BgJQD1AKBAYBUFVXW1dSB1YGUFFUBwMNVAJQWw157b")
+	window.open("./notfound.html")
 });
 $(document).on('click','div#facebook',function(){
 	document.getElementById("au").pause();
-	window.open("https://www.facebook.com/nguyen.dang.tlu")
+	window.open("./notfound.html")
 });
 $(document).on('click','div#linkedIn',function(){
 	document.getElementById("au").pause();
